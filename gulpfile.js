0cdabd577f0be4c0c9de8d50bf38a8180a5dc77b
sergii_maniuk <sergii.maniuk@gmail.com>
2019-06-18 21:32:24 +0300

    add files

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var sourcemaps = require('gulp-sourcemaps');
+var gulpif = require('gulp-if');
+var argv = require('yargs').argv;
+var del = require("del");
+
+function browserTask() {
+  browserSync({
+    server: {baseDir: "./"},
+    browser: ["chrome"]
+  });
+}
+exports.browserTask = browserTask;
+
+function bsReload() {
+  browserSync.reload();
+}
+exports.bsReload = bsReload;
+
+ function styles() {
+  return gulp.src(['content/scss/checkout/**/*.scss'])
+        .pipe(plumber({
+          errorHandler: function (error) {
+            console.log(error.message);
+            this.emit('end');
+        }}))
+        .pipe(gulpif(!argv.prod,sourcemaps.init({loadMaps: true})))
+        .pipe(sass())
+        .pipe(gulpif(argv.prod,autoprefixer({browsers: ['last 200 versions'],cascade: false})))
+        .pipe(gulpif(argv.prod,rename({suffix: '.min'})))
+        .pipe(gulpif(argv.prod,minifycss()))
+        .pipe(gulp.dest('content/css/'))
+        .pipe(gulpif(!argv.prod,sourcemaps.write('.')))
+        .pipe(gulp.dest('content/css/'))
+        .pipe(browserSync.reload({stream:true}))
+}
+exports.styles = styles;
+
+function html() {
+  return gulp.src('./*.html')
+    .pipe(browserSync.reload({stream:true}))
+}
+exports.html = html;
+
+function clean() {
+  return del(["./Content/css/"]);
+}
+exports.clean = clean;
+
+function watchFiles() {
+  gulp.watch("content/scss/checkout/**/*.scss", styles);
+  gulp.watch("./*.html", html);
+}
+
+const build = gulp.series(styles);
+const watch = gulp.parallel(watchFiles, browserTask);
+exports.watch = watch;
+exports.build = build;
+exports.default = build;
