f77f58c67b316585757bc67f53a90ac3e17dbbc4
Sergey Manyuk <maniuk-s@foxtrot.ua>
2020-04-01 11:10:25 +0300

    fox

--- src/js/app.js
+++ src/js/app.js
@@ -1,89 +1,55 @@
-(function($) {
-  $(function() {
-    $("ul.report-tabs__caption").on("click", "li:not(.active)", function() {
-      $(this)
-        .addClass("active")
-        .siblings()
-        .removeClass("active")
-        .closest("div.report-tabs")
-        .find("div.report-tabs__content")
-        .removeClass("active")
-        .eq($(this).index())
-        .addClass("active");
-    });
+// (function($) {
+//   $(function() {
+//     $("ul.report-tabs__caption").on("click", "li:not(.active)", function() {
+//       $(this)
+//         .addClass("active")
+//         .siblings()
+//         .removeClass("active")
+//         .closest("div.report-tabs")
+//         .find("div.report-tabs__content")
+//         .removeClass("active")
+//         .eq($(this).index())
+//         .addClass("active");
+//     });
+//   });
+// })(jQuery);
+
+$(document).ready(function(){
+
+   $('select').select2({
+        width: '100%',
+        minimumResultsForSearch: -1
+   });
+
+
+
+  $('.carousel').slick({
+      dots: false,
+      slidesPerRow: 3,
+      arrows: true,
+      infinity: false,
+      rows: 2,
+      responsive: [
+      {
+        breakpoint: 560,
+        settings: {
+          slidesPerRow: 1,
+          rows: 1
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesPerRow: 2,
+          rows: 1
+        }
+      }
+    ]
   });
-})(jQuery);
 
-$(document).ready(function($) {
-    $('.stellarnav').stellarNav({
-        breakpoint: 1280
-    }); 
-    $('#indexBanner').owlCarousel({
-      margin: 0,
-      nav: true,
-      responsiveClass:true,
-      loop: false,
-      items : 1,
-      nav: false,
-      dots: false
-    })
+  $('[data-toggle="datepicker"]').datepicker();
 
-    $('.slider-brand').owlCarousel({
-      margin: 0,
-      nav: true,
-      responsiveClass:true,
-      loop: false,
-      items : 5,
-      nav: true,
-      dots: false,
-      responsive: {
-        0: {
-          items: 1
-        },
-        560: {
-          items: 2
-        },
-        769: {
-          items: 3
-        },
-        1100: {
-          items: 4
-        },
-        1300: {
-          items: 5
-        }
-      }
-    })
 
-    var slideDef = $('.slider-default'); 
-     slideDef.owlCarousel({
-      margin: 0,
-      autoHeight:true,
-      nav: true,
-      responsiveClass:true,
-      loop: true,
-      items : 3,
-      margin: 23,
-      stagePadding: 4,
-      nav: true,
-      dots: false,
-      responsive: {
-        0: {
-          items: 1
-        },
-        740: {
-          items: 2
-        },
-        769: {
-          items: 2
-        },
-        900: {
-          items: 3
-        },
-        1500: {
-          items: 3
-        }
-      }
-    })
-});
 
+  
+}); 
